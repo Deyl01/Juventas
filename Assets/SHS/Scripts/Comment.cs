ebde155b462337ad750a72cc1d2e5e09c1f83cb4
qRuptung <fjqxjd@gmail.com>
2021-09-09 20:21:22 +0900

    TXT에서 리뷰 불러오기 -완

--- Assets/SHS/Scripts/Comment.cs
+++ Assets/SHS/Scripts/Comment.cs
@@ -6,16 +6,49 @@ using System;
 
 public class Comment : MonoBehaviour
 {
-    string[] commentList;
+    public string[] commentListPositive;
+    public string[] commentListNeutral;
+    public string[] commentListNegative;
 
-
-
-    public void ReadFromList()
+//    게임 시작에만 쓸 스크립트
+    public void CommentReadFromList()
     {
-        commentList = File.ReadAllLines(Application.dataPath + "/SHS" + "/Resources" + "/commentList.txt");
+        TextAsset commentFile = Resources.Load("commentList") as TextAsset;
+        StringReader stringReader = new StringReader(commentFile.text);
 
-        print(commentList[1]);
+        while (stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+
+            if (line == null)
+                break;
+
+            commentArray(int.Parse(line.Split(',')[1]),line.Split(',')[0]);
+
+        }
+        stringReader.Close();
+//        Debug.Log("1");
     }
+    void commentArray(int Type, string commntText) // 택스트 태그를 보고 분류 - 스파게티 코드를 짠거 같다.........
+    {
+//        print(commentListPositive.Length);
+        switch (Type) {
+            case 1:
+                commentListPositive[commentListPositive.Length -1] = commntText;
+                Array.Resize(ref commentListPositive, commentListPositive.Length + 1);
+                break;
+            case 0:
+                commentListNeutral[commentListNeutral.Length - 1] = commntText;
+                Array.Resize(ref commentListNeutral, commentListNeutral.Length + 1);
+                break;
+            case -1:
+                commentListNegative[commentListNegative.Length - 1] = commntText;
+                Array.Resize(ref commentListNegative, commentListNegative.Length + 1);
+                break;
+        }
+//        Debug.Log(commntText);
+    }
+
 
 
     //^태스트용
