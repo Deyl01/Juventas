27f70b6a93128558ae9cbf9202d34f9415c03aaf
setak482 <yumsan1305@gmail.com>
2021-09-24 02:15:05 +0900

    저장 후 커밋
    비어있는 스크립트 파일

---
+++ Assets/CJK/Script/foodlist.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class foodlist : MonoBehaviour
+{
+    class bg_basic{
+
+    }
+    class bg_cheese{
+
+    }
+}
